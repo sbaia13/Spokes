3a8df9a893a2eaf94bb41eaab993a466f0f7b6b9
sbaia13 <117842419+sbaia13@users.noreply.github.com>
2023-09-08 12:26:33 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
     storage_account_name = "backendgithubworkflow1"
     container_name       = "tfstate"
     key                  = "SpokeProd.tfstate"
-    subscription_id = "c435e127-e11a-4d08-9385-fea28f616dcc"
+    subscription_id = "77be15c3-5849-4192-956e-e693d7ae0897"
   }
   required_providers {
     azurerm = {
